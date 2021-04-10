9d8a966a5fc4784e485e1d762f1f826110d24fac
Totagi <supreet.totagi@rakuten.com>
2021-04-10 12:49:34 +0800

    error handling and retry mechanism

--- part_1_api.py
+++ part_1_api.py
@@ -1,19 +1,39 @@
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 def make_call(offset):
+    retry_strategy = Retry(
+        total=3,
+        backoff_factor=1,
+        status_forcelist=[429, 500, 502, 503, 504],
+        method_whitelist=['GET']
+    )
+    http = requests.Session()
+    http.mount('https://', HTTPAdapter(max_retries=retry_strategy))
+
     url = 'https://eservices.mas.gov.sg/api/action/datastore/search.json'
     resource_id = '3a5b732e-9490-4629-a398-d0c414204ee0'
-    res = requests.get(url, params={'resource_id': resource_id, 'limit': 100, 'offset': offset}).json()
-    return res
+
+    try:
+        r = http.get(url, params={'resource_id': resource_id, 'limit': 100, 'offset': offset})
+        r.raise_for_status()
+        if r.status_code == requests.codes.ok:
+            return r.json()
+    except requests.exceptions.RequestException as error:
+    	print("Error: ", error)
+
 
 def get_records():
     url = 'https://eservices.mas.gov.sg/api/action/datastore/search.json'
     resource_id = '3a5b732e-9490-4629-a398-d0c414204ee0'
 
     weekly_results = {}
+    total_records = 0
     first_page = make_call(0)
-    weekly_results[first_page['result']['records'][0]['end_of_week']] = first_page['result']['records'][0]
-    total_records = int(first_page['result']['total'])
+    if first_page is not None:
+        weekly_results[first_page['result']['records'][0]['end_of_week']] = first_page['result']['records'][0]
+        total_records = int(first_page['result']['total'])
 
     for offset in range(100, total_records, 100):
         res = make_call(offset)
@@ -22,6 +42,6 @@ def get_records():
 
     return weekly_results
 
-records = get_records()
-for key in records.keys():
-    print(key)
+# records = get_records()
+# for key in records.keys():
+#     print(key)
